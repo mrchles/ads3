81a57f077b40b276ea43cc65c3c3353af95ec7c9
iless <ilessmirass@gmail.com>
2026-04-27 11:02:32 +0500

    bubble sort quick sort and binary search

---
+++ src/Sorter.java
@@ -0,0 +1,77 @@
+import java.util.Random;
+
+
+public class Sorter {
+    public void BubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public void quickSort(int[] arr) {
+        quickSortRecursive(arr, 0, arr.length - 1);
+    }
+
+    private void quickSortRecursive(int[] arr, int start, int end) {
+        if (start >= end) return;
+
+        int pivotIndex = partition(arr, start, end);
+
+        quickSortRecursive(arr, start, pivotIndex - 1);
+        quickSortRecursive(arr, pivotIndex + 1, end);
+    }
+
+    private int partition(int[] arr, int start, int end) {
+        int pivot = arr[end]; // опорный элемент
+        int smallerIndex = start;
+
+        for (int i = start; i < end; i++) {
+            if (arr[i] < pivot) {
+
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[smallerIndex];
+                arr[smallerIndex] = temp;
+
+                smallerIndex++;
+            }
+        }
+
+        int temp = arr[smallerIndex];
+        arr[smallerIndex] = arr[end];
+        arr[end] = temp;
+
+        return smallerIndex;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
